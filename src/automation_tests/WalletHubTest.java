d1e1079519dfbecb4b4569423453390caca783f9
ratmirasanov <ratmir.asanov.7@gmail.com>
2017-11-18 11:59:22 +0200

    Made more readable the text of review.

--- src/automation_tests/WalletHubTest.java
+++ src/automation_tests/WalletHubTest.java
@@ -113,7 +113,15 @@ public class WalletHubTest {
         }
 
         // Submit a review.
-        write_review_page.fill_form_and_submit("Health", "5","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+        write_review_page.fill_form_and_submit("Health", "5",
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+                        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
+                        "when an unknown printer took a galley of type and scrambled it to make a type " +
+                        "specimen book. It has survived not only five centuries, but also the leap into " +
+                        "electronic typesetting, remaining essentially unchanged. It was popularised in " +
+                        "the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " +
+                        "and more recently with desktop publishing software like Aldus PageMaker including " +
+                        "versions of Lorem Ipsum.");
         System.out.println("Test 2: Review is successfully submitted.");
 
         // Wait for 3 seconds.
